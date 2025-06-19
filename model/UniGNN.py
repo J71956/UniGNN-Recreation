57ac9292a86ba8d63ab3cb8186b36fdfbbfa674d
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-06-19 11:43:02 +0800

    yes

---
+++ model/UniGNN.py
@@ -0,0 +1,407 @@
+import torch
+import torch.nn as nn, torch.nn.functional as F
+
+
+import math 
+
+from torch_scatter import scatter
+from torch_geometric.utils import softmax
+
+
+# NOTE: can not tell which implementation is better statistically 
+
+def glorot(tensor):
+    if tensor is not None:
+        stdv = math.sqrt(6.0 / (tensor.size(-2) + tensor.size(-1)))
+        tensor.data.uniform_(-stdv, stdv)
+
+def normalize_l2(X):
+    """Row-normalize  matrix"""
+    rownorm = X.detach().norm(dim=1, keepdim=True)
+    scale = rownorm.pow(-1)
+    scale[torch.isinf(scale)] = 0.
+    X = X * scale
+    return X
+
+
+
+# v1: X -> XW -> AXW -> norm
+class UniSAGEConv(nn.Module):
+
+    def __init__(self, args, in_channels, out_channels, heads=8, dropout=0., negative_slope=0.2):
+        super().__init__()
+        # TODO: bias?
+        self.W = nn.Linear(in_channels, heads * out_channels, bias=False)
+        
+        self.heads = heads
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.negative_slope = negative_slope
+        self.dropout = dropout
+        self.args = args
+
+    def __repr__(self):
+        return '{}({}, {}, heads={})'.format(self.__class__.__name__,
+                                             self.in_channels,
+                                             self.out_channels, self.heads)
+
+    def forward(self, X, vertex, edges):
+        N = X.shape[0]
+        
+        # X0 = X # NOTE: reserved for skip connection
+
+        X = self.W(X)
+
+        Xve = X[vertex] # [nnz, C]
+        Xe = scatter(Xve, edges, dim=0, reduce=self.args.first_aggregate) # [E, C]
+
+        Xev = Xe[edges] # [nnz, C]
+        Xv = scatter(Xev, vertex, dim=0, reduce=self.args.second_aggregate, dim_size=N) # [N, C]
+        X = X + Xv 
+
+        if self.args.use_norm:
+            X = normalize_l2(X)
+
+        # NOTE: concat heads or mean heads?
+        # NOTE: normalize here?
+        # NOTE: skip concat here?
+
+        return X
+
+
+
+# v1: X -> XW -> AXW -> norm
+class UniGINConv(nn.Module):
+
+    def __init__(self, args, in_channels, out_channels, heads=8, dropout=0., negative_slope=0.2):
+        super().__init__()
+        self.W = nn.Linear(in_channels, heads * out_channels, bias=False)
+        
+        self.heads = heads
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.negative_slope = negative_slope
+        self.dropout = dropout
+        self.eps = nn.Parameter(torch.Tensor([0.]))
+        self.args = args 
+        
+    def __repr__(self):
+        return '{}({}, {}, heads={})'.format(self.__class__.__name__,
+                                             self.in_channels,
+                                             self.out_channels, self.heads)
+
+
+    def forward(self, X, vertex, edges):
+        N = X.shape[0]
+        # X0 = X # NOTE: reserved for skip connection
+        
+        # v1: X -> XW -> AXW -> norm
+        X = self.W(X) 
+
+        Xve = X[vertex] # [nnz, C]
+        Xe = scatter(Xve, edges, dim=0, reduce=self.args.first_aggregate) # [E, C]
+        
+        Xev = Xe[edges] # [nnz, C]
+        Xv = scatter(Xev, vertex, dim=0, reduce='sum', dim_size=N) # [N, C]
+        X = (1 + self.eps) * X + Xv 
+
+        if self.args.use_norm:
+            X = normalize_l2(X)
+
+
+        
+        # NOTE: concat heads or mean heads?
+        # NOTE: normalize here?
+        # NOTE: skip concat here?
+
+        return X
+
+
+
+# v1: X -> XW -> AXW -> norm
+class UniGCNConv(nn.Module):
+
+    def __init__(self, args, in_channels, out_channels, heads=8, dropout=0., negative_slope=0.2):
+        super().__init__()
+        self.W = nn.Linear(in_channels, heads * out_channels, bias=False)        
+        self.heads = heads
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.negative_slope = negative_slope
+        self.dropout = dropout
+        self.args = args 
+        
+    def __repr__(self):
+        return '{}({}, {}, heads={})'.format(self.__class__.__name__,
+                                             self.in_channels,
+                                             self.out_channels, self.heads)
+
+    def forward(self, X, vertex, edges):
+        N = X.shape[0]
+        degE = self.args.degE
+        degV = self.args.degV
+        
+        # v1: X -> XW -> AXW -> norm
+        
+        X = self.W(X)
+
+        Xve = X[vertex] # [nnz, C]
+        Xe = scatter(Xve, edges, dim=0, reduce=self.args.first_aggregate) # [E, C]
+        
+        Xe = Xe * degE 
+
+        Xev = Xe[edges] # [nnz, C]
+        Xv = scatter(Xev, vertex, dim=0, reduce='sum', dim_size=N) # [N, C]
+        
+        Xv = Xv * degV
+
+        X = Xv 
+        
+        if self.args.use_norm:
+            X = normalize_l2(X)
+
+        # NOTE: skip concat here?
+
+        return X
+
+
+
+# v2: X -> AX -> norm -> AXW 
+class UniGCNConv2(nn.Module):
+
+    def __init__(self, args, in_channels, out_channels, heads=8, dropout=0., negative_slope=0.2):
+        super().__init__()
+        self.W = nn.Linear(in_channels, heads * out_channels, bias=True)        
+        self.heads = heads
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.negative_slope = negative_slope
+        self.dropout = dropout
+        self.args = args 
+        
+    def __repr__(self):
+        return '{}({}, {}, heads={})'.format(self.__class__.__name__,
+                                             self.in_channels,
+                                             self.out_channels, self.heads)
+
+    def forward(self, X, vertex, edges):
+        N = X.shape[0]
+        degE = self.args.degE
+        degV = self.args.degV
+
+        # v3: X -> AX -> norm -> AXW 
+
+        Xve = X[vertex] # [nnz, C]
+        Xe = scatter(Xve, edges, dim=0, reduce=self.args.first_aggregate) # [E, C]
+        
+        Xe = Xe * degE 
+
+        Xev = Xe[edges] # [nnz, C]
+        Xv = scatter(Xev, vertex, dim=0, reduce='sum', dim_size=N) # [N, C]
+        
+        Xv = Xv * degV
+
+        X = Xv 
+
+        if self.args.use_norm:
+            X = normalize_l2(X)
+
+
+        X = self.W(X)
+
+
+        # NOTE: result might be slighly unstable
+        # NOTE: skip concat here?
+
+        return X
+
+
+
+class UniGATConv(nn.Module):
+
+    def __init__(self, args, in_channels, out_channels, heads=8, dropout=0., negative_slope=0.2, skip_sum=False):
+        super().__init__()
+        self.W = nn.Linear(in_channels, heads * out_channels, bias=False)
+        
+        self.att_v = nn.Parameter(torch.Tensor(1, heads, out_channels))
+        self.att_e = nn.Parameter(torch.Tensor(1, heads, out_channels))
+        self.heads = heads
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.attn_drop  = nn.Dropout(dropout)
+        self.leaky_relu = nn.LeakyReLU(negative_slope)
+        self.skip_sum = skip_sum
+        self.args = args
+        self.reset_parameters()
+
+    def __repr__(self):
+        return '{}({}, {}, heads={})'.format(self.__class__.__name__,
+                                             self.in_channels,
+                                             self.out_channels, self.heads)
+
+    def reset_parameters(self):
+        glorot(self.att_v)
+        glorot(self.att_e)
+
+    def forward(self, X, vertex, edges):
+        H, C, N = self.heads, self.out_channels, X.shape[0]
+        
+        # X0 = X # NOTE: reserved for skip connection
+
+        X0 = self.W(X)
+        X = X0.view(N, H, C)
+
+        Xve = X[vertex] # [nnz, H, C]
+        Xe = scatter(Xve, edges, dim=0, reduce=self.args.first_aggregate) # [E, H, C]
+
+
+        alpha_e = (Xe * self.att_e).sum(-1) # [E, H, 1]
+        a_ev = alpha_e[edges]
+        alpha = a_ev # Recommed to use this
+        alpha = self.leaky_relu(alpha)
+        alpha = softmax(alpha, vertex, num_nodes=N)
+        alpha = self.attn_drop( alpha )
+        alpha = alpha.unsqueeze(-1)
+
+
+        Xev = Xe[edges] # [nnz, H, C]
+        Xev = Xev * alpha 
+        Xv = scatter(Xev, vertex, dim=0, reduce='sum', dim_size=N) # [N, H, C]
+        X = Xv 
+        X = X.view(N, H * C)
+
+        if self.args.use_norm:
+            X = normalize_l2(X)
+
+        if self.skip_sum:
+            X = X + X0 
+
+        # NOTE: concat heads or mean heads?
+        # NOTE: skip concat here?
+
+        return X
+
+
+
+
+__all_convs__ = {
+    'UniGAT': UniGATConv,
+    'UniGCN': UniGCNConv,
+    'UniGCN2': UniGCNConv2,
+    'UniGIN': UniGINConv,
+    'UniSAGE': UniSAGEConv,
+}
+
+
+
+class UniGCNIIConv(nn.Module):
+    def __init__(self, args, in_features, out_features):
+        super().__init__()
+        self.W = nn.Linear(in_features, out_features, bias=False)
+        self.args = args
+        
+        # Learnable alpha and beta parameters
+        self.alpha = nn.Parameter(torch.tensor(0.1))
+        self.beta = nn.Parameter(torch.tensor(0.5))
+        
+        # Layer normalization (no additional args needed)
+        if args.use_norm:
+            self.norm = nn.LayerNorm(out_features)
+        
+        # Residual connection (automatically handles dimension mismatch)
+        if in_features != out_features:
+            self.res_linear = nn.Linear(in_features, out_features)
+        
+        # Initialize weights properly
+        nn.init.xavier_uniform_(self.W.weight)
+        if hasattr(self, 'res_linear'):
+            nn.init.xavier_uniform_(self.res_linear.weight)
+            if self.res_linear.bias is not None:
+                nn.init.zeros_(self.res_linear.bias)
+
+    def forward(self, X, vertex, edges, X0):
+        N = X.shape[0]
+        degE = self.args.degE
+        degV = self.args.degV
+        
+        # Edge aggregation
+        Xve = X[vertex]  # [nnz, C]
+        Xe = scatter(Xve, edges, dim=0, reduce=self.args.first_aggregate)  # [E, C]
+        Xe = Xe * degE
+        
+        # Vertex aggregation
+        Xev = Xe[edges]  # [nnz, C]
+        Xv = scatter(Xev, vertex, dim=0, reduce='sum', dim_size=N)  # [N, C]
+        Xv = Xv * degV
+        
+        X = Xv
+        
+        # Residual connection
+        if hasattr(self, 'res_linear'):
+            residual = self.res_linear(X)
+        else:
+            residual = X
+        X = X + residual
+        
+        # Normalization
+        if hasattr(self, 'norm'):
+            X = self.norm(X)
+        
+        # Combined with initial features using learnable parameters
+        Xi = (1-self.alpha) * X + self.alpha * X0
+        X = (1-self.beta) * Xi + self.beta * self.W(Xi)
+        
+        return X
+
+class UniGCNII(nn.Module):
+    def __init__(self, args, nfeat, nhid, nclass, nlayer, nhead, V, E):
+        super().__init__()
+        self.V = V 
+        self.E = E 
+        nhid = nhid * nhead
+        
+        # Using SiLU (Swish) activation by default - no args needed
+        self.act = nn.SiLU()
+        
+        self.input_drop = nn.Dropout(args.input_drop)
+        self.dropout = nn.Dropout(args.dropout)
+        
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(torch.nn.Linear(nfeat, nhid))
+        
+        for _ in range(nlayer):
+            self.convs.append(UniGCNIIConv(args, nhid, nhid))
+        
+        self.convs.append(torch.nn.Linear(nhid, nclass))
+        
+        # Parameter groups for regularization
+        self.reg_params = list(self.convs[1:-1].parameters())
+        self.non_reg_params = list(self.convs[0:1].parameters()) + list(self.convs[-1:].parameters())
+        
+        # Initialize all weights properly
+        for conv in self.convs:
+            if isinstance(conv, nn.Linear):
+                nn.init.xavier_uniform_(conv.weight)
+                if conv.bias is not None:
+                    nn.init.zeros_(conv.bias)
+
+    def forward(self, x):
+        V, E = self.V, self.E 
+        x = self.dropout(x)
+        x = self.act(self.convs[0](x))
+        x0 = x  # Save initial features
+        
+        for i, con in enumerate(self.convs[1:-1]):
+            # Dynamic dropout based on layer depth
+            dropout_rate = self.dropout.p * (i / len(self.convs[1:-1]))
+            x = F.dropout(x, p=dropout_rate, training=self.training)
+            
+            x = con(x, V, E, x0)
+            x = self.act(x)
+            x = self.dropout(x)
+        
+        x = self.convs[-1](x)
+        return F.log_softmax(x, dim=1)
+
+
+
